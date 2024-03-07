f74df893a0f5da2b1122228c5dc61fcdffcab30e
Dilpreet Grover <92905896+dfordp@users.noreply.github.com>
2024-03-07 20:36:32 +0530

    Update user.router.js

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,13 +1,13 @@
 import express from "express";
 
 import {getAllUsers,getUser,deleteUser,updateUser} from '../controllers/user.controller.js';
-import {isLoggedIn} from "../middleware/index.js"
+import {isLoggedIn, isOwner} from "../middleware/index.js"
 
 
 const router = express.Router();
 
 router.route('/getUsers').get(isLoggedIn,getAllUsers);
-router.route('/getUser/:id').get(isLoggedIn,getUser);
+router.route('/getUser/:id').get(isLoggedIn,isOwner,getUser);
 router.route('/deleteUser/:id').delete(deleteUser);
 router.route('/updateUser/:id').patch(updateUser);
 
