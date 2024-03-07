fe03d76d8ac66707d2b18d867f12766a77aa36f2
Dilpreet Grover <92905896+dfordp@users.noreply.github.com>
2024-03-07 12:26:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import userRoute from "./routers/user.router.js"
 import taskRoute from "./routers/task.router.js"    
 
 const app=express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true
+  }));
 app.use(express.json());
 
 
