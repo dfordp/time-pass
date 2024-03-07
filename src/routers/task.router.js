e3ff5811b3517c56198e77914d3c8476a0f4d307
Dilpreet Grover <92905896+dfordp@users.noreply.github.com>
2024-03-07 20:40:34 +0530

    Update task.router.js

--- src/routers/task.router.js
+++ src/routers/task.router.js
@@ -1,14 +1,15 @@
 import express from "express";
 
-import {getAllUsers,getUser,deleteUser,updateUser} from '../controllers/user.controller.js';
-import {isLoggedIn, isOwner} from "../middleware/index.js"
-
+import { getAllTasks, getTask, createNewTask, deleteTask, updateTask, getTasksByUser } from "../controllers/task.controller.js";
+import { isLoggedIn ,isOwner } from "../middleware/index.js";
 
 const router = express.Router();
 
-router.route('/getUsers').get(isLoggedIn,getAllUsers);
-router.route('/getUser/:id').get(isLoggedIn,isOwner,getUser);
-router.route('/deleteUser/:id').delete(isOwner,deleteUser);
-router.route('/updateUser/:id').patch(isOwner,updateUser);
+router.route('/createTask').post(isLoggedIn ,createNewTask);
+router.route('/getTasks').get(isLoggedIn, getAllTasks);
+router.route('/getTask/:id').get(isLoggedIn, getTask);
+router.route('/deleteTask/:id').delete(isLoggedIn, deleteTask);
+router.route('/updateTask/:id').patch(isLoggedIn, updateTask);
+router.route('/getTasksByUser/:userId').get(isLoggedIn, getTasksByUser);
 
 export default router;
